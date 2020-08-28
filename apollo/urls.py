b0a6ad31c55e335fa28ed900270f32c585f26c99
maqi <925447599@qq.com>
2020-08-28 11:42:24 +0800

    完成A等级的开发

--- apollo/urls.py
+++ apollo/urls.py
@@ -4,4 +4,5 @@ from apollo import views
 
 urlpatterns = [
     path('login/',views.login),# 配置登录路由
+    path('sefe/',views.safe_a),# 配置登录路由
 ]
--- apollo/views.py
+++ apollo/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 def login(request):
     return HttpResponse('index')
+
+
+def safe_a(request):
+    return HttpResponse('实现了A等级的安全级别')
+
+
+
